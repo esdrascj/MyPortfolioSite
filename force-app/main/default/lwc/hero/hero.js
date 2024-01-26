55e4a7fc945d4483b957ea6e6f4489ed17eeb7e8
esdras_conceicao <esdrasrubro@outlook.com>
2024-01-26 19:15:18 -0300

    Hero sections add in layout

---
+++ force-app/main/default/lwc/hero/hero.js
@@ -0,0 +1,8 @@
+import { LightningElement } from 'lwc';
+import IMAGES from '@salesforce/resourceUrl/Images'
+
+
+export default class Hero extends LightningElement {
+
+    imageURL = 'https://power-drive-2710-dev-ed.scratch.lightning.force.com/cms/media/MCG4P4CGKFVFFAHLS5TD7MCT2KEE?cb=05T8N000002JZUg';
+}
